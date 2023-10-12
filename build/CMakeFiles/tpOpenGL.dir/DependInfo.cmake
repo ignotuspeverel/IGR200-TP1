98f7ea9ad80f6888655a647f7c4a0eb87ee4d909
ignotuspeverel <ignotus@sjtu.edu.cn>
2023-10-12 15:22:41 +0200

    first commit

---
+++ build/CMakeFiles/tpOpenGL.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/d/slam/postgraduate/gde1smt1/CG/telecom/TP1/glad/src/glad.c" "/mnt/d/slam/postgraduate/gde1smt1/CG/telecom/TP1/build/CMakeFiles/tpOpenGL.dir/glad/src/glad.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "_MY_OPENGL_IS_33_"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/d/slam/postgraduate/gde1smt1/CG/telecom/TP1/main.cpp" "/mnt/d/slam/postgraduate/gde1smt1/CG/telecom/TP1/build/CMakeFiles/tpOpenGL.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_MY_OPENGL_IS_33_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/tpOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tpOpenGL.dir/glad/src/glad.c.o"
+  "CMakeFiles/tpOpenGL.dir/main.cpp.o"
+  "tpOpenGL"
+  "tpOpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/tpOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
